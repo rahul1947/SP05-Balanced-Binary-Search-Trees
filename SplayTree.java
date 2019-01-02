cf8dcc3ed7c8c800a5cdf570bcf52dd92859ec08
Rahul Nalawade <rahul.nalawade@utdallas.edu>
2019-01-01 18:10:15 -0600

    first commit

---
+++ SplayTree.java
@@ -0,0 +1,10 @@
+package rsn170330.sp05;
+
+// Starter code for Splay Tree
+import java.util.Comparator;
+
+public class SplayTree<T extends Comparable<? super T>> extends BinarySearchTree<T> {
+    SplayTree() {
+	super();
+    }
+}
